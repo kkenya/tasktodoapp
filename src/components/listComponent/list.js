87155c98ea866ef12d4453c79660187a91c206ab
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2019-05-30 21:37:30 +0900

    とりあえず完成

--- src/components/listComponent/list.js
+++ src/components/listComponent/list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const List = ({ todos, deleteTd }) => {
+const List = ({ todos, deleteTd, set, deletedelements, allelements, activeelements }) => {
   const listItems = todos.map((todo, index) =>
     <div>
       <li key={index}>{todo}</li>
@@ -8,9 +8,14 @@ const List = ({ todos, deleteTd }) => {
     </div>
   );
   return (
-    <ul>
-      {listItems}
-    </ul>
+    <div>
+      <ul>
+        {listItems}
+      </ul>
+      <button onClick={() => set(deletedelements)}>完了</button>
+      <button onClick={() => set(allelements)}>全て</button>
+      <button onClick={() => set(activeelements)}>未完了</button>
+    </div>
   );
 }
 
